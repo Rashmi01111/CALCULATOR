8b49ea4a85db13d96055fbb681e4a5099eee14ca
Rashmi01111 <154067431+Rashmi01111@users.noreply.github.com>
2024-02-03 20:07:12 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,27 @@
+
+
+
+let buttons= document.querySelectorAll('button');
+let input= document.querySelector('input');
+
+for(let button of buttons){
+    button.addEventListener('click',function(e){
+      let btnText=e.target.innerText;
+if(btnText==='C'){
+    input.value='';
+}
+else if(btnText === '='){
+    try{input.value= eval(input.value);   
+}
+catch{
+    input.value='gaddari'
+}}
+else{
+    input.value +=btnText;
+}
+
+    })
+}
+
+
+
